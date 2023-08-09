1f2a84d24fc42a9f8f2c7411afba505731129727
Mark Ryan Longhas <yanixmrml@gmail.com>
2023-08-10 02:39:35 +0800

    Test-2: Another test for Dependency Injection

--- lab/12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
+++ lab/12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
@@ -1,5 +1,17 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import rewards.RewardNetwork;
+import rewards.internal.RewardNetworkImpl;
+import rewards.internal.account.AccountRepository;
+import rewards.internal.account.JdbcAccountRepository;
+import rewards.internal.restaurant.JdbcRestaurantRepository;
+import rewards.internal.restaurant.RestaurantRepository;
+import rewards.internal.reward.JdbcRewardRepository;
+import rewards.internal.reward.RewardRepository;
+
 import javax.sql.DataSource;
 
 /**
@@ -42,9 +54,36 @@ import javax.sql.DataSource;
  *   not an implementation.
  */
 
+@Configuration
 public class RewardsConfig {
 
 	// Set this by adding a constructor.
 	private DataSource dataSource;
+
+	public RewardsConfig(DataSource dataSource){
+		this.dataSource = dataSource;
+	}
+
+	@Bean RewardNetwork rewardNetwork(){
+		return new RewardNetworkImpl(accountRepository(),restaurantRepository(),rewardRepository());
+	}
+
+	@Bean AccountRepository accountRepository(){
+		 JdbcAccountRepository jdbcAccountRepository = new JdbcAccountRepository();
+		 jdbcAccountRepository.setDataSource(dataSource);
+		 return jdbcAccountRepository;
+	}
+
+	@Bean RestaurantRepository restaurantRepository(){
+		JdbcRestaurantRepository jdbcRestaurantRepository = new JdbcRestaurantRepository();
+		jdbcRestaurantRepository.setDataSource(dataSource);
+		return jdbcRestaurantRepository;
+	}
+
+	@Bean RewardRepository rewardRepository(){
+		JdbcRewardRepository jdbcRewardRepository = new JdbcRewardRepository();
+		jdbcRewardRepository.setDataSource(dataSource);
+		return jdbcRewardRepository;
+	}
 
 }
